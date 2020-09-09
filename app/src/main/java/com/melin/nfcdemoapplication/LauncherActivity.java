18776931df456d6ad88295087b3636af611d7d26
Pravesh Chapagain <spravesh1818@gmail.com>
2020-09-09 18:14:49 +0545

    NFC demo v2

---
+++ app/src/main/java/com/melin/nfcdemoapplication/LauncherActivity.java
@@ -0,0 +1,29 @@
+package com.melin.nfcdemoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        button=(Button)findViewById(R.id.launchNfcActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LauncherActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
